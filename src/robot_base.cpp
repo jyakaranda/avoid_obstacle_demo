bb017e1869cc2474b1dbcd8a812b44cacb77a55a
jyakaranda <jyakaranda@jyakaranda-Ubuntu.16.04>
2018-04-05 23:34:22 +0800

    init

---
+++ src/robot_base.cpp
@@ -0,0 +1,122 @@
+#include "ros/ros.h"
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Twist.h>
+#include "avoid_obstacle_demo/obstacles.h"
+#include "avoid_obstacle_demo/obstacle.h"
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+#define PI 3.1415926
+#define DEG2RAD(x) ((x)/180.0*PI)
+#define RAD2DIST(x) (0.0855 / (abs(sin(x))) )
+
+bool param_debug;
+double param_min_ob_dist;
+double param_max_speed;
+double param_angle_interval;
+double param_min_angle;
+double param_max_angle;
+
+double theta;
+double min_angle, max_angle;
+
+bool is_ok;
+
+ros::Publisher pub_cmd_vel;
+
+
+void cb_obstacle_info(avoid_obstacle_demo::obstacles ob){
+    static bool locked = false;
+    static ros::Rate rate(1);
+    bool flag = false;
+    vector<avoid_obstacle_demo::obstacle> obs_v;
+    ROS_INFO("cb_obstacle_info");
+
+    if(locked){
+        ROS_INFO("cb_obstacle_info locked");
+        return;
+    } else {
+        locked = true;
+    }
+
+    for(int i = 0; i < ob.size; i++){
+        if((ob.angle[i]+PI) >= (PI-theta) && (ob.angle[i]+PI) <= (theta+PI) && ob.dist[i] > param_min_ob_dist){
+            continue;
+        }
+        if(((ob.angle[i]+PI) >= (PI-theta) && (ob.angle[i]+PI) <= (theta+PI) && ob.dist[i] < param_min_ob_dist) || (ob.angle[i]+PI > min_angle && ob.angle[i]+PI < max_angle && RAD2DIST(ob.angle[i]) > ob.dist[i])){
+            
+            ROS_INFO("detected obstacle %d in dist: %f m, angle: %f rad; RAD2DIST: %f m", obs_v.size(), ob.dist[i], ob.angle[i] + PI, RAD2DIST(ob.angle[i]));
+            avoid_obstacle_demo::obstacle tmp;
+            tmp.dist = ob.dist[i];  tmp.angle = ob.angle[i] + PI;
+            obs_v.push_back(tmp);
+        }
+    }
+    
+    if(obs_v.size() == 0){
+        ROS_INFO("there is no detected obstacle, move forward...");
+        geometry_msgs::Twist twist;
+        twist.angular.x = 0;  twist.angular.y = 0;  twist.angular.z = 0;
+        twist.linear.x = param_max_speed;  twist.linear.y = 0;  twist.linear.z = 0;
+        pub_cmd_vel.publish(twist);
+        ros::spinOnce();
+        rate.sleep();
+        
+    } else{
+        ROS_INFO("there are %d detected obstacle points", obs_v.size());
+        geometry_msgs::Twist twist;
+        twist.angular.x = 0;  twist.angular.y = 0;
+        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
+        twist.angular.z = 0;
+        pub_cmd_vel.publish(twist);
+
+        rate.sleep();
+
+        if(obs_v[obs_v.size()/2].angle > PI){
+            twist.angular.z = -DEG2RAD(param_angle_interval);
+        } else{
+            twist.angular.z = DEG2RAD(param_angle_interval);
+        }
+
+
+        pub_cmd_vel.publish(twist);
+        ros::spinOnce();
+        rate.sleep();
+    }
+    locked = false;
+}
+
+void init(){
+    is_ok = false;
+}
+
+int main(int argc,char *argv[]){
+
+    ros::init(argc, argv, "robot_base_node");
+    ros::NodeHandle n;
+    ros::NodeHandle nh("~");
+
+    nh.param<bool>("debug", param_debug, false);
+    nh.param<double>("min_ob_dist", param_min_ob_dist, 0.0);
+    nh.param<double>("max_speed", param_max_speed, 0.2);
+    nh.param<double>("min_angle", param_min_angle, 135.0);
+    nh.param<double>("max_angle", param_max_angle, 225.0);
+    nh.param<double>("angle_interval", param_angle_interval, 30.0);
+
+    theta = atan(0.0855/param_max_speed);
+    min_angle = DEG2RAD(param_min_angle);
+    max_angle = DEG2RAD(param_max_angle);
+    
+    pub_cmd_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 50);
+    
+    ros::Subscriber sub_obstacle = n.subscribe<avoid_obstacle_demo::obstacles>("obstacle_info", 50, cb_obstacle_info);
+
+
+    ROS_INFO("robot_base ready");
+
+    ros::spin();
+
+    return 0;
+
+}
---
+++ src/sensor_source.cpp
@@ -0,0 +1,138 @@
+#include "ros/ros.h"
+#include "avoid_obstacle_demo/obstacles.h"
+#include "avoid_obstacle_demo/obstacle.h"
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Header.h>
+#include <string>
+#include <vector>
+
+// #include <rosbag/bag.h>
+// #include <rosbag/vie.h>
+// #include <boost/foreach.hpp>
+
+#define PI 3.1415926
+#define DEG2RAD(x) ((x)*PI/180.0)
+
+ros::Time current_time, last_time;
+// ros::Duration obstacle_update_interval;
+// double param_min_angle, param_max_angle;
+// double min_angle, max_angle;
+static bool locked = false;
+
+
+class SensorSource{
+    public:
+        SensorSource();
+        void init();
+        void cb_laser_scan(const sensor_msgs::LaserScan::ConstPtr &laserMsg);
+        void cb_camera();
+        void cb_odom(const nav_msgs::Odometry::ConstPtr odomMsg);
+
+    private:
+        ros::NodeHandle n_;
+        ros::NodeHandle nh_;
+        ros::Subscriber sub_laser_scan;
+        ros::Subscriber sub_odom;
+        ros::Publisher pub_obstacle;
+
+        ros::Duration obstacle_update_interval;
+        double param_min_angle;
+        double param_max_angle;
+        double min_angle;
+        double max_angle;
+};
+
+SensorSource::SensorSource():nh_("~"){
+    init();
+}
+
+void SensorSource::init(){
+    double tmp;
+    nh_.param<double>("obstacle_update_interval", tmp, 0.1);
+    obstacle_update_interval.fromSec(tmp);
+    nh_.param<double>("min_angle", param_min_angle, -45.0);
+    nh_.param<double>("max_angle", param_max_angle, 45.0);
+    min_angle = DEG2RAD(param_min_angle) - PI;
+    max_angle = DEG2RAD(param_max_angle) - PI;
+
+    sub_laser_scan = n_.subscribe<sensor_msgs::LaserScan>("scan", 100, boost::bind(&SensorSource::cb_laser_scan, this, _1));
+    sub_odom = n_.subscribe<nav_msgs::Odometry>("odom", 50, boost::bind(&SensorSource::cb_odom, this, _1));
+    pub_obstacle = n_.advertise<avoid_obstacle_demo::obstacles>("obstacle_info", 50);
+}
+
+void SensorSource::cb_laser_scan(const sensor_msgs::LaserScan::ConstPtr &laserMsg){
+
+    ROS_INFO("wtf %d", laserMsg->header.seq);
+    //sensor_msgs::LaserScanPtr laserMsg(new sensor_msgs::LaserScan());
+    static ros::Time last_scan(0, 0);
+    static int count = 0;
+    ROS_INFO("cb_laser_scan: %d", count);
+    if(laserMsg == 0){
+        return;
+    }
+    // 至少以obstacle_update_interval的时间间隔更新障碍物信息
+    if((laserMsg->header.stamp - last_scan) > obstacle_update_interval){
+        ROS_INFO("cb_laser_scan: %d", count);
+        int li = 0, ri = laserMsg->ranges.size() - 1;
+        if((laserMsg->angle_min > max_angle) || (laserMsg->angle_max < min_angle)){
+            // 扫描角度不在[min_angle, max_angle]区间，默认前进方向无障碍
+            return;
+        }
+        li = (laserMsg->angle_min <= min_angle) ? li : (int)((laserMsg->angle_min-min_angle)/laserMsg->angle_increment);
+        ri = (laserMsg->angle_max >= max_angle) ? ri : (laserMsg->ranges.size() - (int)((max_angle-laserMsg->angle_max)/laserMsg->angle_increment));
+        avoid_obstacle_demo::obstacles::Ptr obs(new avoid_obstacle_demo::obstacles());
+        obs->size = ri - li;
+        obs->dist.resize(obs->size);
+        obs->angle.resize(obs->size);
+        obs->angle_increment = laserMsg->angle_increment;
+
+        for(int i = li; i <= ri; i++){
+            obs->dist[i - li] = laserMsg->ranges[i];
+            obs->angle[i - li] = laserMsg->angle_min + (i * laserMsg->angle_increment);
+            ROS_INFO("laser obstacle %d - dist: %.2f, angle: %.4f", i, obs->dist[i-li], obs->angle[i-li]);
+        }
+        last_scan = laserMsg->header.stamp;
+        count++;
+        obs->header.seq = count;
+        obs->header.stamp = last_scan;
+        // TODO 得到雷达扫描的障碍物数组
+        if(!locked){
+            locked = true;
+            ROS_INFO("locked !!!");
+            pub_obstacle.publish(obs);
+            locked = false;
+        }
+    } else{
+        ROS_INFO("interval: %lf", (laserMsg->header.stamp - last_scan).toSec());
+    }
+}
+
+void SensorSource::cb_camera(){
+
+}
+
+void SensorSource::cb_odom(const nav_msgs::Odometry::ConstPtr odomMsg){
+
+}
+
+
+int main(int argc, char *argv[]){
+
+    ros::init(argc, argv, "sensor_source");
+
+    // TODO ros::Subscriber sub_camera = n_.subscribe<>();
+    // ros::Subscriber sub_odom = n_.subscribe<>
+
+    SensorSource sensors;
+
+
+    ROS_INFO("sensor_source ready");
+
+    ros::Rate rate(10);
+
+    ros::spin();
+
+
+    return 0;
+}
